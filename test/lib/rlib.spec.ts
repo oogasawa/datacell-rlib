a60b48fc9ceee211511d1304f39622f2b211cf01
Osamu Ogasawara <osamu.ogasawara@gmail.com>
2020-11-12 07:06:16 +0900

    Refactor to use jest framework

--- test/lib/rlib.spec.ts
+++ test/lib/rlib.spec.ts
@@ -2,9 +2,6 @@
 import * as rlib from "../../src/lib/rlib";
 
 
-import * as chai from 'chai';
-const expect = chai.expect;
-
 import * as log4js from 'log4js';
 const logger = log4js.getLogger();
 // logger.level = 'debug';
@@ -13,11 +10,11 @@ const logger = log4js.getLogger();
 
 describe('Array functions', () => {
 
-    context("seq()", () => {
+    describe("seq()", () => {
 
         it('rlib.seq(0) should return [] array.', () => {
             const result: number[] = rlib.seq(0);
-            expect(result).to.be.empty;
+            expect(result.length).toEqual(0);
         });
 
 
@@ -26,20 +23,20 @@ describe('Array functions', () => {
             const ans: number[] = [0, 1, 2, 3, 4];
 
             for (let i = 0; i < ans.length; i++) {
-                expect(ans[i]).equals(result[i]);
+                expect(ans[i]).toEqual(result[i]);
             }
         });
 
     });
 
-    context("seq()", () => {
+    describe("seq()", () => {
         it('should generate a series.', () => {
 
             const result: number[] = rlib.seq(5);
             const ans: number[] = [0, 1, 2, 3, 4];
 
             for (let i = 0; i < ans.length; i++) {
-                expect(ans[i]).equals(result[i]);
+                expect(ans[i]).toEqual(result[i]);
             }
         });
 
@@ -47,14 +44,14 @@ describe('Array functions', () => {
 
 
 
-    context("toArray()", () => {
+    describe("toArray()", () => {
         it('should generate a series from given function.', () => {
 
             const result: number[] = rlib.toArray(() => { return 1; }, 5);
             const ans: number[] = [1, 1, 1, 1, 1];
 
             for (let i = 0; i < ans.length; i++) {
-                expect(ans[i]).equals(result[i]);
+                expect(ans[i]).toEqual(result[i]);
             }
         });
 
